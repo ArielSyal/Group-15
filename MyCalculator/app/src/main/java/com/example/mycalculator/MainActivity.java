83e69dd002cb25ab4c20c1b7812c0b5feff6801e
Huiting Huang <solachan0156@gmail.com>
2025-09-24 23:05:38 -0400

    part1 completed

--- MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.java
+++ MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mycalculator;
+import static com.example.mycalculator.MathEval.eval;
+
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,7 +17,7 @@ import java.math.BigDecimal;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn1, btn2, btn_clear, btn_plus, btn_equal;
+    Button btn0, btn1, btn2, btn3, btn_clear, btn_plus, btn_minus, btn_dot, btn_equal;
     TextView text_display;
 
     // This is to evaluate the math expression
@@ -24,10 +27,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        btn0 = (Button) findViewById(R.id.btn0);
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
         btn_plus = (Button) findViewById(R.id.btn_plus);
+        btn_minus = (Button) findViewById(R.id.btn_minus);
+        btn_dot = (Button) findViewById(R.id.btn_dot);
         btn_equal = (Button) findViewById(R.id.btn_equal);
         btn_clear = (Button) findViewById(R.id.btn_clear);
         text_display = (TextView) findViewById(R.id.textview_input_display);
@@ -36,9 +42,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setClickListeners() {
+        btn0.setOnClickListener(this);
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
         btn_plus.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_dot.setOnClickListener(this);
         btn_equal.setOnClickListener(this);
         btn_clear.setOnClickListener(this);
     }
@@ -46,19 +56,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn0:
+                addNumber("0");
+                break;
             case R.id.btn1:
                 addNumber("1");
                 break;
             case R.id.btn2:
                 addNumber("2");
                 break;
+            case R.id.btn3:
+                addNumber("3");
+                break;
             case R.id.btn_plus:
                 addNumber("+");
                 break;
+            case R.id.btn_minus:
+                addNumber("-");
+                break;
+            case R.id.btn_dot:
+                addNumber(".");
+                break;
             case R.id.btn_equal:
-                String result = null;
+                String result;
                 try {
                     result = evaluate(text_display.getText().toString());
+                    Log.d("DEBUG", "Evaluating: " + result);
                     text_display.setText(result);
                 } catch (Exception e) {
                     text_display.setText("Error");
@@ -71,9 +94,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private String evaluate(String expression) throws Exception {
-        String result = evaluate(expression);
-        BigDecimal decimal = new BigDecimal(result);
-        return decimal.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString();
+
+        String result = eval(expression);
+        //BigDecimal decimal = new BigDecimal(result);
+        //decimal.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString()
+        return result;
+
     }
 
     private void addNumber(String number) {
