4b49dc328c9b121c801a60a416aff48e290d4433
Ariel Syal <arielsyal@Ariels-MacBook-Pro.local>
2025-10-08 19:57:41 -0400

    Updated BankAccount, ChequingAccount, LoanAccount, SavingsAccount, and InvestmentAccount

--- SEGAssignment2/InvestmentAccount.java
+++ SEGAssignment2/InvestmentAccount.java
@@ -1,11 +1,18 @@
 public class InvestmentAccount extends BankAccount {
     private String nickName;
 
-    public InvestmentAccount(int accountNumber, double balance, Client owner, String nickName) {
-        super(accountNumber, balance, owner);
+    public InvestmentAccount(long id, double balance, Client owner, String nickName) {
+        super(id, balance, owner);
         this.nickName = nickName;
     }
 
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
     @Override
     public String displayInfo() {
         return "InvestmentAccount[id=" + getId() + ", balance=" + getBalance() + ", nickName=" + nickName + "]";
--- SEGAssignment2/SavingsAccount.java
+++ SEGAssignment2/SavingsAccount.java
@@ -2,12 +2,23 @@ public class SavingsAccount extends BankAccount {
     private String nickName;
     private double interestRate;
 
-    public SavingsAccount(int accountNumber, double balance, Client owner, String nickName, double interestRate) {
-        super(accountNumber, balance, owner);
+    public SavingsAccount(long id, double balance, Client owner, String nickName, double interestRate) {
+        super(id, balance, owner);
         this.nickName = nickName;
         this.interestRate = interestRate;
     }
-
+    public String getNickName() {
+        return nickName;
+    }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    public double getInterestRate() {
+        return interestRate;
+    }
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
     @Override
     public String displayInfo() {
         return "SavingsAccount[id=" + getId() + ", balance=" + getBalance() + ", nickName=" + nickName + ", interestRate=" + interestRate + "]";
